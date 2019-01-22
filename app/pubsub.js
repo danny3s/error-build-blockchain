017478b62961769a5bc261f254a0b3e1124c11e8
Daniel <dahusa.92@gmail.com>
2019-01-22 16:23:11 -0500

    server

--- app/pubsub.js
+++ app/pubsub.js
@@ -1,9 +1,9 @@
 const PubNub = require('pubnub');
 
 const credentials = {
-  publishKey: 'pub-c-e81cc4',
-  subscribeKey: 'sub-e92',
-  secretKey: 'sec-c-OZiOWVm'
+  publishKey: 'pub-c-ec30f7ec-578f-4aa2-81c8-59077fb942c4',
+  subscribeKey: 'sub-c-eda4e664-027b-11e9-a39c-e60c31199fb2',
+  secretKey: 'sec-c-OWQwMTg1MGMtY2U2YS00ZmVlLWE1YmEtOTVmMWZmN2ZiOWVm'
 };
 
 const CHANNELS = {
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const request = require('request');
 const Blockchain = require('./blockchain');
+const path = require('path');
 const bodyParser = require('body-parser');
 const PubSub = require('./app/pubsub');
 const TransactionPool = require('./wallet/transaction-pool');
@@ -20,6 +21,7 @@ const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
 
 
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'client')));
  
 app.get('/api/blocks', (req, res) => {
     res.json(blockchain.chain);
@@ -80,6 +82,10 @@ app.get('/api/mine-transactions', (req, res) => {
       address,
       balance: Wallet.calculateBalance({ chain: blockchain.chain, address })
     });
+  });
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/index.html'));
   });
   
 
